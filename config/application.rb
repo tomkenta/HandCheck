b70e237dc836ae080fd5d1e370cab612d3d507f2
tomkenta <tomkenta2014@gmail.com>
2017-11-02 02:39:02 +0900

    modify view

--- config/application.rb
+++ config/application.rb
@@ -21,8 +21,8 @@ module Handcheck
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    config.paths.add File.join('app', 'apis'), glob: File.join('**', '*.rb')
-    config.autoload_paths += Dir[Rails.root.join('app', 'apis', '*')]
+    config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
+    config.autoload_paths += Dir[Rails.root.join('app', 'api', '*')]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
